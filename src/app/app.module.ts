55fec9f58a4832be66dc5dae264d97ff982023ba
Guest User <Guest@epicodus-20.local>
2017-11-01 09:20:52 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
